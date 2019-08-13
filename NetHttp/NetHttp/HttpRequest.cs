0453240f4f9ff2dcb7548317a3959815ce915b45
OnurOzkan22 <onur-ozkan@hotmail.de>
2019-08-13 15:42:43 +0200

    some tests

--- NetHttp/NetHttp/HttpRequest.cs
+++ NetHttp/NetHttp/HttpRequest.cs
@@ -110,5 +110,10 @@ namespace NetHttp
                     throw new Exception("This http method doesn't exist or it's currently not supported");
             }
         }
+
+        public void TTT()
+        {
+
+        }
     }
 }
